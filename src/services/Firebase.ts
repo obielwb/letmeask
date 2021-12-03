2b74b75284688b36497eff7bb551e92b09814a1a
Gabriel Willian <obielwb@gmail.com>
2021-12-03 14:39:17 -0300

    fix firebase config

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,7 +1,6 @@
-import firebase from 'firebase/app';
-
-import 'firebase/auth';
-import 'firebase/database'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/database'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
