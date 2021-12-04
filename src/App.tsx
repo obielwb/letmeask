da22f3a218d46b0c449a7c1542ea4f00384bb6d5
Gabriel Willian <obielwb@gmail.com>
2021-12-04 13:34:44 -0300

    set signIn with Google configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+
+import { createContext } from 'react';
+
+import { firebase, auth } from './services/Firebase';
+
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
@@ -5,13 +11,50 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles/global.scss'
 
+type User = {
+  id: string, 
+  name: string, 
+  avatar: string
+}
+
+type AuthContextType = {
+  user: User | undefined, 
+  signInWithGoogle: () => void
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
 function App() {
+  const [user, setUser] = useState<User>();
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('User is missing displayName or photoURL');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName, 
+          avatar: photoURL
+        });
+      }
+    })
+  }
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms/new" element={<NewRoom />} />
-      </Routes>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
