b08969881e92aff604711d106911be1b758d96e1
Gabriel Willian <obielwb@gmail.com>
2021-12-04 14:20:50 -0300

    remove contexts from App and redirect to another place

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,19 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { createContext } from 'react';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
-import { firebase, auth } from './services/Firebase';
-
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './styles/global.scss'
-
-type User = {
-  id: string, 
-  name: string, 
-  avatar: string
-}
-
-type AuthContextType = {
-  user: User | undefined, 
-  signInWithGoogle: () => void
-}
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      if (result.user) {
-        const { displayName, photoURL, uid } = result.user;
-
-        if (!displayName || !photoURL) {
-          throw new Error('User is missing displayName or photoURL');
-        }
-
-        setUser({
-          id: uid,
-          name: displayName, 
-          avatar: photoURL
-        });
-      }
-    })
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/rooms/new" element={<NewRoom/>} />
         </Routes>
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
