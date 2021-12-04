4e93ceb7f104d03aca13a3d2242df804f6a93546
Gabriel Willian <obielwb@gmail.com>
2021-12-04 14:22:18 -0300

    fix imports

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
+import logoImg from '../assets/images/logo.svg';
 
+import { Button } from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
-import { Button } from '../components/Button';
 
 export function NewRoom() {
+  // const { user } = useAuth()
+
   return (
     <div id="page-auth">
       <aside>
@@ -19,17 +22,20 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-
-          <form action="">
+          <form>
             <input 
               type="text"
-              placeholder="Nome da sala" />
-            <Button type="submit" >Criar sala</Button>
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
           </form>
-
-          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
-  );
+  )
 }
