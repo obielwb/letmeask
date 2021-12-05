984c0077302df889eef0c4457ccdd1cd89a11606
Gabriel Willian <obielwb@gmail.com>
2021-12-05 12:32:21 -0300

    create Room router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
+
+import './styles/global.scss';
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/rooms/new" element={<NewRoom/>} />
+          <Route path="/rooms/:id" element={<Room/>} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
